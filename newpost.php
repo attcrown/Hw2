081fdffa0ea3a495e3c13b3c534c5a846ff6412c
attcrwon <ittatting@gmail.com>
2022-11-01 15:32:13 +0700

    database 1-11-65

--- newpost.php
+++ newpost.php
@@ -11,22 +11,65 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <title>Document</title>
 </head>
 <body>
-
-    <h1 style="text-align: center;">Webboard Server Bullvpn</h1>
-    <hr>
-    <table style="width: 40%" align="center">
-        <tr><td colspan="2"><b>ผู้ใช้</b> : <?php echo $_SESSION['username'] ?></td></tr>
-        <tr><td><b>หมวดหมู่</b> :</td><td><select name="category">
-                                            <option value="allboard">เรื่องทั่วไป</option>
-                                            <option value="foodboard">เรื่องอาหาร</option>
-                                            <option value="lisboard">เรื่องเรียน</option>
-                                        </select></td></tr>
-        <tr><td><b>หัวข้อ</b> :</td><td><input type="password" name="newpwd" size="35"></td></tr>
-        <tr><td><b>เนื้อหา</b> :</td><td><textarea style="width: 80%;" name="textboard"></textarea></td></tr>
-        <tr><td></td><td colspan="2" style="text-align: left;"><input type="submit" value="บันทึกข้อความ"></td></tr>
-    </table>
+    <div class="container mt-5">
+        <h1 style="text-align: center;">Webboard VPN bull</h1>
+        <?php include "nav.php"; ?>
+        <br>
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-6">
+                <div class="card text-dark bg-white border-info">
+                    <div class="card-header bg-info text-white">ตั้งกระทู้ใหม่</div>
+                    <div class="card-body">
+                        <form action="newpost_save.php" method="post">
+                            <div class="row mb-3">
+                                <label class="col-lg-3 col-form-label">หมวดหมู่ :</label>
+                                <div class="col-lg-9">
+                                    <select name="category" class="form-select">
+                                        <?php
+                                            $conn=new PDO("mysql:host=localhost;dbname=webboard;charset-utf8", "root", "");
+                                            $sql="SELECT * FROM category";
+                                            foreach($conn->query($sql) as $row){
+                                                echo "<option value=".$row['id'].">".$row['name']."</option>";
+                                            }
+                                            $conn=null;
+                                        ?>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-lg-3 col-form-label">หัวข้อ :</label>
+                                <div class="col-lg-9">
+                                    <input type="text" name="topic" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-lg-3 col-form-label">เนื้อหา :</label>
+                                <div class="col-lg-9">
+                                    <textarea class="form-control" name="comment" rows="8" required></textarea>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <center>
+                                        <button type="submit" class="btn btn-info btn-sm text-white">
+                                            <i class="bi bi-caret-right-square me-1"></i>บันทึกข้อความ</button>
+                                    </center>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3"></div>    
+        </div>
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,32 +1,97 @@
-<?php 
+<?php
 session_start();
-    if(isset($_SESSION['id'])){
-        header("location:index.php");
-        die();
-    }
+if (isset($_SESSION['id'])) {
+    header("location:index.php");
+    die();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <title>register</title>
 </head>
+
 <body>
-    <h1 style="text-align: center;">สมัครสมาขิก</h1>
-    <hr>
-<table style="border: 2px solid black; width: 40%" align="center">
-    <tr><td colspan="2" style="background-color: #6CD;">กรอกข้อมูล</td></tr>
-    <tr><td>ชื่อบัญชี :</td><td><input type="text" name="newuser" size="40"></td></tr>
-    <tr><td>รหัสผ่าน :</td><td><input type="password" name="newpwd" size="40"></td></tr>
-    <tr><td>ชื่อ-นามสกุล :</td><td><input type="text" name="newname" size="40"></td></tr>
-    <tr><td rowspan="3">เพศ :</td><td><input type="radio" name="sexm">ชาย</td></tr>
-    <tr><td><input type="radio" name="sexg">หญิง</td></tr>
-    <tr><td><input type="radio" name="sexall">อื่นๆ</td></tr>
-    <tr><td>อีเมล :</td><td><input type="email" name="email" size="40"></td></tr>
-    <tr><td colspan="2" style="text-align: center;"><input type="submit" value="register"></td></tr>
-</table>
-<p style="text-align: center;"><a href="index.php">กลับไปหน้าหลัก</a></p>
+    <div class="container mt-5">
+        <h1 style="text-align: center;">Webboard VPN Bull</h1>
+        <?php include "nav.php"; ?>
+        <br>
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-6">
+                <div class="card text-dark bg-white border-primary">
+                    <div class="card-header bg-primary text-white">กรอกข้อมูล</div>
+                    <div class="card-body">
+                        <form action="register_save.php" method="POST">
+                            <div class="row mb-3">
+                                <label for="" class="col-md-3 col-form-label">ชื่อบัญชี:</label>
+                                <div class="col-md-9">
+                                    <input type="text" name="login" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label for="" class="col-md-3 col-form-label">รหัสผ่าน:</label>
+                                <div class="col-md-9">
+                                    <input type="password" name="pwd" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label for="" class="col-md-3 col-form-label">ชื่อ-นามสกุล:</label>
+                                <div class="col-md-9">
+                                    <input type="text" name="name" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label for="" class="col-md-3 col-form-label pt-0">เพศ:</label>
+                                <div class="col-md-9">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="gender" id="flexRadioDefault1" value="m">
+                                        <label class="form-check-label" for="flexRadioDefault1">
+                                            ชาย
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="gender" id="flexRadioDefault2" value="f">
+                                        <label class="form-check-label" for="flexRadioDefault2">
+                                            หญิง
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="gender" id="flexRadioDefault3" value="o" checked>
+                                        <label class="form-check-label" for="flexRadioDefault3">
+                                            อื่นๆ
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label for="" class="col-md-3 col-form-label">อีเมล:</label>
+                                <div class="col-md-9">
+                                    <input type="text" name="email" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label for="" class="col-md-3 col-form-label"></label>
+                                <div class="col-md-9">
+                                    <button class="btn btn-primary btn-sm" type="submit"><i class="bi bi-save"></i> สมัครสมาชิค</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3"></div>
+        </div>
+    </div>
 </body>
+
 </html>
